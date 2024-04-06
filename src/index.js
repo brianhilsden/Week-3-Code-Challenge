ff2b22dc3e1684a66f2742f94f2e8a142189d305
Brian Omondi <brianhilsden@gmail.com>
2024-04-07 00:47:31 +0300

    added remote server to gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const showtime = document.getElementById("showtime");
 const ticketsLeft = document.getElementById("ticket-num");
 const buyTickets = document.getElementById("buy-ticket")
 const numOfTicketsBought = document.getElementById("tickets_bought")
-const filmUrl = "http://localhost:3000/films";
+const filmUrl = "https://json-server-flatdango.onrender.com/films";
 
 
 // Fetches and lists film titles from the server
@@ -147,7 +147,7 @@ function purchaseFilmTickets(film) {
         });
 
       //POST request to add bought ticket(s) to the tickets endpoint
-      fetch("http://localhost:3000/tickets", {
+      fetch("https://json-server-flatdango.onrender.com/tickets", {
         method: "POST",
         headers: {
           // Headers sent with the request
