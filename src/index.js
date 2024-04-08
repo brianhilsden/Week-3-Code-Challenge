d5ce093a36f7804391d490eb7e6ce8f810469070
Brian Omondi <brianhilsden@gmail.com>
2024-04-08 16:35:26 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const ticketsLeft = document.getElementById("ticket-num");
 const buyTickets = document.querySelector("div button");
 const ticketsBought = document.getElementById("tickets-bought")
 const movieTickets = document.getElementById("movie-tickets")
-const filmUrl = "https://json-server-flatdango.onrender.com/films";
+const filmUrl = "https://json-server-ogg2.onrender.com/films";
 
 // Fetches and lists film titles from the server
 function getFilmsData() {
@@ -136,7 +136,7 @@ function purchaseFilmTickets(film) {
         });
 
       //POST request to add bought ticket(s) to the tickets endpoint
-      fetch("https://json-server-flatdango.onrender.com/tickets", {
+      fetch("https://json-server-ogg2.onrender.com/tickets", {
         method: "POST",
         headers: {
           // Headers sent with the request
